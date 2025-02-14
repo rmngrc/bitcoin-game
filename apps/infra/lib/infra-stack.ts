549d6e663bab1e42d7a665d5d334545694cc10aa
Ramón García <rmngrc@proton.me>
2025-02-15 00:34:30 +0100

    feat: add static site construct

--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { StageConfig } from "../bin/stages";
+import { StaticSiteConstruct } from "./static-site-construct";
 
 export class BitcoinGameInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: StageConfig) {
     super(scope, id, props);
+
+    new StaticSiteConstruct(this, "BTCStaticSite", props);
   }
 }
