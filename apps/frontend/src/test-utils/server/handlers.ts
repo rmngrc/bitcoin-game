182ea70cc3ccb8c57b4f21454107c50e8b2cb10a
Ramón García <rgarcia@merlyn.org>
2025-02-15 03:16:10 +0100

    chore: add handler for the price and start endpoints

--- apps/frontend/src/test-utils/server/handlers.ts
+++ apps/frontend/src/test-utils/server/handlers.ts
@@ -1,3 +1,36 @@
-import { http } from "msw";
+import { http, HttpResponse } from "msw";
+import { v4 as uuidv4 } from "uuid";
 
-export const handlers = [http.get("/start", async () => {})];
+// In memory store. It will be reset every time the server is restarted.
+const score = 0;
+
+export const handlers = [
+  http.get(`${import.meta.env.VITE_API_URL}/price`, async () => {
+    return HttpResponse.json({ symbol: "BTCUSDT", price: (getRandomPrice() / 100).toString() });
+  }),
+  http.get(`${import.meta.env.VITE_API_URL}/start`, ({ cookies }) => {
+    if (!cookies.sessionId) {
+      const sessionId = uuidv4();
+      return HttpResponse.json(
+        { score: 0 },
+        {
+          status: 200,
+          headers: {
+            "Set-Cookie": `sessionId=${sessionId}; Path=/; HttpOnly; Secure; Max-Age=${
+              60 * 60 * 24 * 365 * 10
+            }`,
+          },
+        },
+      );
+    }
+
+    return HttpResponse.json({ score }, { status: 200 });
+  }),
+  http.post(`${import.meta.env.VITE_API_URL}/score`, () => {
+    throw new Error("Not implemented");
+  }),
+];
+
+function getRandomPrice() {
+  return Math.random() * (9400000 - 9300000) + 9300000;
+}
