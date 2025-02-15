e82acc96a062f8df29cfaf3934ba9727dfdfdbf2
Ramón García <rgarcia@merlyn.org>
2025-02-15 03:24:23 +0100

    feat: use GuessControls component from Game component

--- apps/frontend/src/components/Game.tsx
+++ apps/frontend/src/components/Game.tsx
@@ -1,5 +1,7 @@
 import { PriceDisplay } from "@/components/PriceDisplay";
 import { useGetBitcoinPrice } from "@/hooks/useGetBitcoinPrice";
+import { Guess } from "@/types";
+import { GuessControls } from "./GuessControls";
 import { ScoreBoard } from "./ScoreBoard";
 
 interface GameProps {
@@ -13,6 +15,11 @@ export const Game = ({ initialScore = 0 }: GameProps) => {
     <div className="flex flex-col gap-8 items-center justify-center my-8">
       <PriceDisplay price={currentPrice} isLoading={isLoading} />
       <ScoreBoard score={0} />
+      <GuessControls
+        countdown={0}
+        disabled={true}
+        onGuess={async (guess: Guess) => console.log(guess)}
+      />
     </div>
   );
 };
