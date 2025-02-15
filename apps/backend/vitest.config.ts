360824b1b8e25d4cd6c468ee97302721b6eecbe5
Ramón García <rgarcia@merlyn.org>
2025-02-15 02:08:48 +0100

    feat: add env variables for testing

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    reporters: ["verbose"],
+    env: {
+      STAGE: "local",
+      DYNAMODB_TABLE: "test-table",
+    },
     environment: "node",
     onConsoleLog(log) {
       return true;
     },
+    reporters: ["verbose"],
   },
 });
