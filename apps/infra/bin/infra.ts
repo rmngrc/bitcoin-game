08db063405c2ac3edbef6460d94fbdf626d52d2b
Ramón García <rmngrc@proton.me>
2025-02-16 10:03:24 +0100

    refactor: change variable name to reflect real environment name

--- apps/infra/bin/infra.ts
+++ apps/infra/bin/infra.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { BitcoinGameInfraStack } from "../lib/infra-stack";
-import { STAGE_PROD_US_EAST_1 } from "./stages";
+import { STAGE_DEV_US_EAST_1 } from "./stages";
 
 const app = new cdk.App();
 
 // We could add here more stages here if applicable.
 // To deploy a given stage we just need to pass the stack name to cdk deploy.
-new BitcoinGameInfraStack(app, "BTCInfraStack-Dev-UsEast1", STAGE_PROD_US_EAST_1);
+new BitcoinGameInfraStack(app, "BTCInfraStack-Dev-UsEast1", STAGE_DEV_US_EAST_1);
--- apps/infra/bin/stages.ts
+++ apps/infra/bin/stages.ts
@@ -6,7 +6,7 @@ export type StageConfig = {
   stage: string;
 };
 
-export const STAGE_PROD_US_EAST_1: StageConfig = {
+export const STAGE_DEV_US_EAST_1: StageConfig = {
   env: {
     awsAccountId: "913524900415",
     region: "us-east-1",
